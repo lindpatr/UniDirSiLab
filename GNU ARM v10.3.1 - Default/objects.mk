a7ce552dad87e3614ba193131d1b38550dc5dc4f
Patrick <linpat@icloud.com>
2022-08-29 15:33:29 +0200

    Initial version Uni Dir

---
+++ GNU ARM v10.3.1 - Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Users/BEL-LinPat/SimplicityStudio/SDKs/gecko_sdk_2//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg22_gcc_release.a
+
+LIBS := -lgcc -lc -lm -lnosys
+
