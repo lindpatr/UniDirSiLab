2341ab82468447e4b39cd05d22f15260a709339f
Alex <alechenne@gmail.com>
2022-08-30 09:28:57 +0200

    For importation in Simplicity

--- GNU ARM v10.3.1 - Default/objects.mk
+++ GNU ARM v10.3.1 - Default/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := C:/Users/BEL-LinPat/SimplicityStudio/SDKs/gecko_sdk_2//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg22_gcc_release.a
+USER_OBJS := C:/Users/alech/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg22_gcc_release.a
 
 LIBS := -lgcc -lc -lm -lnosys
 
